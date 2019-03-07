2c81be0dfad86d9c62ac4232410b8458d830769c
Enric Cervera <ecervera@uji.es>
2019-03-07 16:14:23 +0100

    roslab

---
+++ roslab_build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -f roslab.dockerfile -t trajectory-optimized-active-search .
---
+++ roslab_run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+docker run --rm \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --runtime=nvidia \
+     -v /usr/local/MATLAB/R2017a:/usr/local/MATLAB/from-host --mac-address=2c:60:0c:d6:50:36 -p 8888:8888 trajectory-optimized-active-search
